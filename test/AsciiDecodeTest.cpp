642eee06d6ff715326f8725bf77321cb66aa391d
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-09-29 16:38:53 +0200

    Added valid tests.

--- test/AsciiDecodeTest.cpp
+++ test/AsciiDecodeTest.cpp
@@ -1,3 +1,15 @@
 #include "gtest/gtest.h"
 
-TEST(AsciiDecode, test) { EXPECT_EQ(false, false); }
+#include "../AsciiDecode.h"
+
+TEST(AsciiDecode, decodePositive)
+{
+    EXPECT_EQ(AsciiDecode::decode("24746211151814964359"), "GREAT");
+    EXPECT_EQ(AsciiDecode::decode("2244"), "0");
+}
+
+TEST(AsciiDecode, decodeNegative)
+{
+    EXPECT_NE(AsciiDecode::decode("24746211151814964359"), "NOT GREAT");
+    EXPECT_NE(AsciiDecode::decode("2244"), "NOT 0");
+}
