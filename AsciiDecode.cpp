7af4a7fac4b66da3c6cba41fd2cd10d04dd85dbf
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-09-29 17:19:43 +0200

    Static analysis fixes.

--- AsciiDecode.cpp
+++ AsciiDecode.cpp
@@ -5,27 +5,27 @@ namespace AsciiDecode
 static const unsigned int SEQUANCE_SIZE{4U};
 static const unsigned int DECIMAL_BASE{10U};
 
-static unsigned char getAsciiCode(unsigned int encodedAsciiAsNumber)
+static char getAsciiCode(unsigned int encodedAsciiAsNumber)
 {
     const unsigned int firstNumber{encodedAsciiAsNumber / 1000 * DECIMAL_BASE +
                                    encodedAsciiAsNumber % 100 / DECIMAL_BASE};
     const unsigned int secondNumber{encodedAsciiAsNumber % 1000 / 100 *
                                         DECIMAL_BASE +
                                     encodedAsciiAsNumber % DECIMAL_BASE};
-    return firstNumber + secondNumber;
+    return static_cast<char>(firstNumber + secondNumber);
 }
 
 std::string decode(const std::string& inputSequence)
 {
     std::string decoded;
-    const unsigned long asciiSymbolsCount{inputSequence.size() / SEQUANCE_SIZE};
-    for (unsigned long i = 0; i < asciiSymbolsCount; ++i)
+    const uint64_t asciiSymbolsCount{inputSequence.size() / SEQUANCE_SIZE};
+    for (uint64_t i = 0; i < asciiSymbolsCount; ++i)
     {
         const std::string encodedAsciiAsString{
             std::string(inputSequence, SEQUANCE_SIZE * i, SEQUANCE_SIZE)};
-        const unsigned int encodedAsciiAsNumber(
-            std::stoi(encodedAsciiAsString));
-        decoded.append(1, getAsciiCode(encodedAsciiAsNumber));
+        const unsigned int encodedAsciiAsNumber{
+            static_cast<unsigned int>(std::stoi(encodedAsciiAsString))};
+        decoded.push_back(getAsciiCode(encodedAsciiAsNumber));
     }
     return decoded;
 }
