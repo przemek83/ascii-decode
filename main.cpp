81e8a334b09ff789041300aaaa8022aa1878f1fe
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-10-02 18:17:01 +0200

    Update main.cpp
    Cleaning main.cpp.

--- main.cpp
+++ main.cpp
@@ -1,35 +1,3 @@
-//  Ascii decode
-//   Convert string of digits into ascii string with following rules:
-//   - Each 4 consecutive digits is encoded 1 ascii char.
-//   - Digit 1 and 3 need to be concatenated to get first number.
-//   - Digit 2 and 4 need to be concatenated to get second number.
-//   - Sum of first and second number is an ascii code.
-//
-//   Decoding example:
-//   input in form of 24746211151814964359 is split into 2474 6211
-//   1518 1496 4359. For each 4 digits group we create first and second number:
-//   2474 : 27 and 44 (sum 71)
-//   6211 : 61 and 21 (sum 82)
-//   1518 : 11 and 58 (sum 69)
-//   1496 : 19 and 46 (sum 65)
-//   4359 : 45 and 39 (sum 84)
-//   ASCII codes (sums) will give encoded string GREAT (71 = G, 82 = R, ...).
-
-// Input:
-//   Per line encoded ascii chars as sequences of 4 digits.
-//
-// Output
-//   Decoded ascii string.
-//
-// Example
-// In:
-//   24746211151814964359
-//   2244
-
-// Out:
-//   GREAT
-//   0
-
 #include <iostream>
 
 #include "AsciiDecode.h"
